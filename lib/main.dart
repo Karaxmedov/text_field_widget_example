f4d4ff256261490acc78375bcbacddd1da055953
Karaxmedov <nicat.karaxmedov.nk@gmail.com>
2023-04-05 17:10:24 +0400

    Added .gitatributes and licence

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    _emailController = TextEditingController(text: "email@email.com");
+    _passwordController = TextEditingController(text: "Password");
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              setState(() {});
+            }),
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
@@ -18,13 +44,29 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Padding(
+                padding: const EdgeInsets.all(8),
+                child: Text(
+                  "Email:${_emailController.text}",
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Text(
+                  "Password ${_passwordController.text}",
+                ),
+              ),
+              Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextField(
+                  controller: _emailController,
                   keyboardType: TextInputType.emailAddress,
                   textInputAction: TextInputAction.next,
                   autofocus: true,
                   onChanged: (value) {
-                    debugPrint(value);
+                    _emailController.value = TextEditingValue(
+                        text: value,
+                        selection:
+                            TextSelection.collapsed(offset: value.length));
                   },
                   onSubmitted: (value) {
                     debugPrint("Submitted value :$value");
@@ -39,12 +81,13 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
                 child: TextField(
+                  controller: _passwordController,
                   keyboardType: TextInputType.text,
                   textInputAction: TextInputAction.next,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: "Password",
                     hintText: "Enter password",
                     border: OutlineInputBorder(
@@ -52,6 +95,12 @@ class MyApp extends StatelessWidget {
                     prefixIcon: Icon(Icons.lock_outline),
                     suffixIcon: Icon(Icons.remove_red_eye_outlined),
                   ),
+                  onChanged: (value) {
+                    _passwordController.value = TextEditingValue(
+                        text: value,
+                        selection:
+                            TextSelection.collapsed(offset: value.length));
+                  },
                 ),
               ),
             ],
